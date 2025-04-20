db21d9e56e1ac03d3caed7cbf3f705986c5aa3a5
Amilaje <choijinho321@gmail.com>
2025-04-21 04:02:46 +0900

    java 연습용 파일 업로드드

---
+++ 최진호/20250409/SelfTest.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class SelfTest {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
