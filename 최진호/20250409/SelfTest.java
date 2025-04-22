c4ee008591843f7a0b89273d3bf97aa7f701f5d7
Amilaje <choijinho321@gmail.com>
2025-04-23 02:16:58 +0900

    Finding GSD _최진호호

--- 최진호/20250409/SelfTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class SelfTest {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        
-    }
-}
