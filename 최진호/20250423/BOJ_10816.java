dda38cde3196ad4b2285be65958812d46e0904b2
Amilaje <choijinho321@gmail.com>
2025-04-30 03:08:03 +0900

    코드 주석처리

--- 최진호/20250423/BOJ_10816.java
+++ 최진호/20250423/BOJ_10816.java
@@ -17,16 +17,23 @@ public class BOJ_10816 {
         for(int i=0;i<m;i++){
             arr2[i] = Integer.parseInt(input_m[i]);
         }
-        Arrays.sort(arr1);
+        //------------------------------------------- 여기까지 선언
+
+        Arrays.sort(arr1); // 이진 탐색을 위한 정렬
         
         StringBuilder sb = new StringBuilder();
         for(int num : arr2){
-            sb.append(bi_search_count(arr1, num)).append(" ");
+            sb.append(bi_search_count(arr1, num)).append(" "); // 출력을 위해 이진 탐색 후 Buffer 입력
         }
 
         System.out.println(sb);
+        sc.close(); // 종료
     }
     public static int bi_search_count(int[] arr, int find){
+        // 2진 탐색은 2회 실시함.
+        // 1회는 왼쪽에서 가장 가까운 Index, 2회는 오른쪽으로 가장 가까운 Index.
+        
+        // 1회차 
         int left = 0;
         int right = arr.length;
         while(left<right){
@@ -34,8 +41,9 @@ public class BOJ_10816 {
             if(arr[mid]<find){left = mid+1;}
             else{right = mid;}
         }
-        int left_idx = left;
+        int left_idx = left; // 중복 카드의 왼쪽 끝
 
+        // 2회차
         left = 0;
         right = arr.length;
         while(left<right){
@@ -43,8 +51,8 @@ public class BOJ_10816 {
             if(arr[mid]<=find){left = mid+1;}
             else{right = mid;}
         }
-        int right_idx = left;
+        int right_idx = left; // 중복 카드의 오른쪽 끝. 
         
-        return right_idx - left_idx ;
+        return right_idx - left_idx ;// 중복카드의 두 끝의 차 = 카드의 수
     }
 }
--- 최진호/20250423/BOJ_7795.java
+++ 최진호/20250423/BOJ_7795.java
@@ -5,7 +5,8 @@ public class BOJ_7795 {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         sc.nextLine();
-        for(int i=0; i<t; i++){
+
+        for(int i=0; i<t; i++){ // t번 반복
             String[] input_size = sc.nextLine().split(" ");
             int size_a = Integer.parseInt(input_size[0]);
             int size_b = Integer.parseInt(input_size[1]);
@@ -15,9 +16,14 @@ public class BOJ_7795 {
             ArrayList<Integer> arr2 = new ArrayList<>(size_b);
             for(String s : input_a){arr1.add(Integer.parseInt(s));}
             for(String s : input_b){arr2.add(Integer.parseInt(s));}
-            arr1 = quick_sort(arr1);
-            arr2 = quick_sort(arr2);
+            //------------------------------------------- 여기까지 선언
+
+            arr1 = quick_sort(arr1); // 이진 탐색을 위한 정렬
+            arr2 = quick_sort(arr2); // 이진 탐색을 위한 정렬
+            
             System.out.println(bi_search(arr1, arr2));
+
+            sc.close(); // 종료
         }
     }
     public static int bi_search(ArrayList<Integer> arr1, ArrayList<Integer> arr2){
@@ -29,6 +35,8 @@ public class BOJ_7795 {
             int left = 0;
             int right = size_b;
             while (left < right) {
+                // 해당 이진 탐색은 mid를 찾고 끝나는 것이 아님.
+                // target 과 가장 오른쪽으로 가까운 index를 찾는 것이 목표
                 int mid = (left + right) / 2;
                 if (arr2.get(mid) < target) {
                     left = mid + 1;
@@ -57,7 +65,7 @@ public class BOJ_7795 {
             }
         }
         ArrayList<Integer> sorted = new ArrayList<>();
-        sorted.addAll(quick_sort(less));
+        sorted.addAll(quick_sort(less)); 
         sorted.add(pivot);
         sorted.addAll(quick_sort(greater));
 
