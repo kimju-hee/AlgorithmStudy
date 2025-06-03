22e394e121fb5d960132928616f25bf00d9d12a9
egg <wkddnwls1010@naver.com>
2025-06-03 20:26:57 +0900

    BOJ_17390 이건 꼭 풀어야 해!

---
+++ 장우진/20250521/BOJ_17390.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int[] prefix = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            prefix[i] = prefix[i - 1] + A[i - 1];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int R = Integer.parseInt(st.nextToken());
+
+            sb.append(prefix[R] - prefix[L - 1]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
