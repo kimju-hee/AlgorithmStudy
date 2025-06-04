281d90dfd9929433d14fc461d80d7712883d8aae
YWS00 <wsyoon2000@gmail.com>
2025-06-04 17:14:58 +0900

    Create BOJ_2167.java

---
+++ 윤원섭/20250521/BOJ_2167.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N + 1][M + 1];
+        int[][] S = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+                S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + A[i][j];
+            }
+        }
+        int K = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int k = 0; k < K; k++) {
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int sum = S[x][y] - S[i - 1][y] - S[x][j - 1] + S[i - 1][j - 1];
+
+            sb.append(sum).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
